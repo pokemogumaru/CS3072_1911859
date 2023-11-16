f9fd95b596512dd1adc265cc9ae90b6a28657a98
James <1911859@brunel.ac.uk>
2023-11-16 17:00:12 +0000

    it seems the hill climber works at last
    problem partially was the 9 to 10 distance was 0 due to a loop mistake. this is now fixed.

--- src/CS3072_1911859.java
+++ src/CS3072_1911859.java
@@ -86,11 +86,12 @@ returns a 1-D array of type double
 		double[][] array_2D = new double[(int) x][(int) x]; //new 2D distance matrix
 		int a = 0; //used to track position in the 1D array
 		double b = 0; //using this to track array_1D values as we use less reads
-		for(int i = 0; i < x-2; i++) { //x-2 because we count from 0 and we ignore 1st 0 value which would be from a city to the same city
+		for(int i = 0; i < x -1; i++) { //x-1 because we ignore 1st 0 value which would be from a city to the same city
 			  for(int j = i+1; j < x; j++) {
 				b = array_1D[a];
 			    array_2D[i][j] = b; //populate 2D values
 			    array_2D[j][i] = b;
+				//System.out.println("b = " + b + ", array_2D[i][j] = " + array_2D[i][j] + ", array_2D[j][i] = " + array_2D[j][i] + ", a = " + a + ", i = " + i + ", j = " + j);
 			    a++; //increment a
 			    
 			  }
--- src/SolveTSP.java
+++ src/SolveTSP.java
@@ -17,7 +17,7 @@ public class SolveTSP { //this class is used for solving TSPs
     tourPrinter(initialSolution); //prints our initial solution for testing
     //double initialCost = generateTourCost(initialSolution, distances); //the cost of our initial solution
     //removed the above line as the initial solution is likely invalid and will appear the best due to 0 values
-    int iterations = 10;
+    int iterations = 30;
     List<Integer> hcSolution = hillClimberSolver(numCities, initialSolution, iterations, distances);
     //hasZeroValue(initialSolution,distances);
     
