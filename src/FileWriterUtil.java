6e0ba116328f61fa3cf97350c4c215f2f18950db
James <1911859@brunel.ac.uk>
2023-11-24 14:02:03 +0000

    added file logger class
    need to test and implement in other classes to use

---
+++ src/FileWriterUtil.java
@@ -0,0 +1,45 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterUtil {
+
+    private BufferedWriter writer;
+    private String filename;
+    private boolean isCsv;
+    
+    public FileWriterUtil(String filename, String filetype) throws IOException {
+        this.filename = filename;
+        this.isCsv = filetype.equalsIgnoreCase("csv");
+        start();
+    }
+
+    public void start() throws IOException { //start by opening file
+        writer = new BufferedWriter(new FileWriter(filename)); 
+    }
+    
+    public void addLineTXT(String text) throws IOException {
+        writer.write(text); //add text to txt file
+        writer.newLine(); //So next time we add the next on new line
+    }
+
+    public void close() throws IOException  { //finish by closing file
+        writer.close();
+    }
+
+    public void addRowCSV(String text) throws IOException { //add string to next row / cell on the right
+        if(!isCsv) { //should only be used by CSV file
+            throw new IOException("Not in CSV mode");
+        }
+        writer.write(text);
+        writer.write(",");
+    }
+
+    public void addColumnCSV(String text) throws IOException { //add string to next line
+        if(!isCsv) {
+            throw new IOException("Not in CSV mode");
+        } 
+        writer.write(text); 
+        writer.newLine();
+    }
+}
