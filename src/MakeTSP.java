260a391b96001ed5dff516417cf9a8c395d24315
James <1911859@brunel.ac.uk>
2024-01-18 16:47:41 +0000

    Update MakeTSP.java

--- src/MakeTSP.java
+++ src/MakeTSP.java
@@ -1,6 +1,8 @@
 
 public class MakeTSP {
 //This class will be used to generate / modify TSPs to minimise / maximise their difficulty
+//input: iterations (int), maximise_difficulty (boolean), TSP[] (double)
+//should make small change to any non 0 value in the 1D tsp or both values in 2D version
 	public static void main(String[] args) {
 		
 
