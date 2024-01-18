07e6cfd3811710d9f683940d489e3215afd5383a
James <1911859@brunel.ac.uk>
2024-01-18 17:49:35 +0000

    Update MakeTSP.java

--- src/MakeTSP.java
+++ src/MakeTSP.java
@@ -3,9 +3,11 @@ public class MakeTSP {
 //This class will be used to generate / modify TSPs to minimise / maximise their difficulty
 //input: iterations (int), maximise_difficulty (boolean), TSP[] (double)
 //should make small change to any non 0 value in the 1D tsp or both values in 2D version
-	public static void main(String[] args) {
-		
-
+	public MakeTSP(double[] distances, boolean MaxOrMin, int iterations)
+	{
+		// do all the heavy lifting in seperate method, add file logging after
+		// step 1 - for iterations do something
+		//step 2 - within loop keep making small changes and compare current fitness to old fitness (likely some methods for this
+		//step 3 - finish with final TSP, best solveTSP result and MST
 	}
-
 }
