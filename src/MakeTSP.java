2ba4d3a44884aeec699ae99e2f628005acc2f42b
James <1911859@brunel.ac.uk>
2024-01-23 18:17:24 +0000

    Update MakeTSP.java

--- src/MakeTSP.java
+++ src/MakeTSP.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 public class MakeTSP {
 //This class will be used to generate / modify TSPs to minimise / maximise their difficulty
-//input: iterations (int), maximise_difficulty (boolean), TSP[] (double)
 //should make small change to any non 0 value in the 1D tsp or both values in 2D version
 	private static FileWriterUtil basicLog; private static boolean UsebasicLog = true;
 	private static FileWriterUtil fullLog; private static boolean UseFullLog = true;
