b18405402e218e878fdf5568854b44b47ce71e73
James <1911859@brunel.ac.uk>
2024-02-10 16:28:35 +0000

    testing

--- src/CS3072_1911859.java
+++ src/CS3072_1911859.java
@@ -25,12 +25,12 @@ public class CS3072_1911859 {
 		
 		//To run the TSP maker. input: TSP, harder (true) / easier (false), iterations for outer HC, repeats (use 1 for 1 run), iterations for inner HC
 		
-		int iterations = 10000;
+		int iterations = 1000;
 		int NumCities = 4;
 		int repeats = 20;
 		MakeTSPsAndPlotWithR(NumCities, true, iterations, repeats, iterations);
 		MakeTSPsAndPlotWithR(NumCities, false, iterations, repeats, iterations);
-		NumCities = 14;
+		NumCities = 100;
 		MakeTSPsAndPlotWithR(NumCities, true, iterations, repeats, iterations); 
 		MakeTSPsAndPlotWithR(NumCities, false, iterations, repeats, iterations);	
 		
