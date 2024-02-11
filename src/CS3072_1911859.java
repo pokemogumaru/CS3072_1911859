79b9416b133251cb9e77d1939b5aef1a5ff7e02f
James <1911859@brunel.ac.uk>
2024-02-11 14:19:52 -0800

    Update CS3072_1911859.java

--- src/CS3072_1911859.java
+++ src/CS3072_1911859.java
@@ -27,7 +27,7 @@ public class CS3072_1911859 {
 		
 		int iterations = 10000; int NumCities = 4; //number of iterations and number of cities
 		int repeats = 20; String type = "SA"; //number of repeats and type of HC to use (HC = basic hill climber) (SA = simulated annealing)
-		int initialTemp = 1000; double coolingRate = 0.8; //used in SA, if HC is selected, these values will not be used.
+		int initialTemp = 100; double coolingRate = 0.99; //used in SA, if HC is selected, these values will not be used.
 		MakeTSPsAndPlotWithR(NumCities, true, iterations, repeats, iterations, type, initialTemp,coolingRate);
 		MakeTSPsAndPlotWithR(NumCities, false, iterations, repeats, iterations,type, initialTemp,coolingRate);
 		NumCities = 14; //repeating with other amount of cities
