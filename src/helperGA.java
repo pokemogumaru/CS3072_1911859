8634ae2ae76a95bb5cdd50e9bd73872431988188
James <1911859@brunel.ac.uk>
2024-02-12 18:00:48 +0000

    Update helperGA.java

--- src/helperGA.java
+++ src/helperGA.java
@@ -6,17 +6,17 @@ public class helperGA {
 	private static ArrayList<String> globalStrings = new ArrayList<>(); //used to store arrays of strings for logging
 	public static double[][] selectParents(double[][] population, double[] distances, int innerIterations) throws IOException {
 		//Chooses parents for crossover
-		String distLengthInnerIter = ("selectParents: distances.length = " + distances.length + " innerIterations = " + innerIterations);
+		String distLengthInnerIter = ("helperGA: selectParents: distances.length = " + distances.length + " innerIterations = " + innerIterations);
 		globalStrings.add(distLengthInnerIter); 
 		//System.out.println(debug);
 	    double[][] parents = new double[2][distances.length];
 	    double[] fitness = calculateFitness(population, distances, innerIterations);
-	    globalStrings.add("selectParents: fitness = " + Arrays.toString(fitness)); //this is a large amount of text to write
+	    globalStrings.add("helperGA: selectParents: fitness = " + Arrays.toString(fitness)); //this is a large amount of text to write
 	    // Roulette wheel selection
 	    parents[0] = selectViaRoulette(population, fitness);
-	    globalStrings.add("selectParents: parents[0] = " + Arrays.toString(parents[0]));
+	    globalStrings.add("helperGA: selectParents: parents[0] = " + Arrays.toString(parents[0]));
 	    parents[1] = selectViaRoulette(population, fitness);
-	    globalStrings.add("selectParents: parents[1] = " + Arrays.toString(parents[1]));
+	    globalStrings.add("helperGA: selectParents: parents[1] = " + Arrays.toString(parents[1]));
 	    MakeTSP.fullLogStrings(globalStrings);//when done, give the arrayList of strings back to be logged
 	    return parents;
 	}
@@ -24,11 +24,12 @@ public class helperGA {
 		//evaluates each member of the population to get an array of fitness scores. Needed for selections and survival
 	    double[] fitness = new double[population.length];
 	    for (int i = 0; i < population.length; i++) {
-	        double[] solution = population[i];
-	        double mst = MakeTSP.GetMST(solution); 
+	        double[] solution = population[i]; // Get the solution for the current member
+	        double mst = MakeTSP.GetMST(solution); // Calculate the minimum spanning tree (MST) for the solution
 	        SolveTSP solver = new SolveTSP(solution,innerIterations);
 	        double tsp = solver.return_solution();
 	        fitness[i] = mst / tsp; 
+	        globalStrings.add("helperGA: calculateFitness: fitness[i] = " + String.valueOf(fitness[i]));
 	    }
 	    //don't call fullLogStrings as not public method
 	    return fitness;
@@ -49,7 +50,7 @@ public class helperGA {
 	private static double sum(double[] array) {double total = 0;for(double value : array) {total += value;}return total;}
 	//iterates through the array and totals up all the values
 
-	public static double[][] survivors(double[][] offspring, double[][] population) {
+	public static double[][] survivors(double[][] offspring, double[][] population) throws IOException {
 		//Determines fittest solutions to carry over. Maintains constant population size
 	    double[][] combined = concatenate(offspring, population);
 	    double[] fitness = getFitness(combined);
@@ -57,7 +58,9 @@ public class helperGA {
 	    double[][] nextGen = new double[population.length][];
 	    for (int i = 0; i < population.length; i++) {
 	        nextGen[i] = combined[indexOf(fitness, i)];
+	        globalStrings.add("helperGA: survivors: nextGen[i] = " + Arrays.toString(nextGen[i]));
 	    }
+	    MakeTSP.fullLogStrings(globalStrings);//when done, give the arrayList of strings back to be logged
 	    return nextGen;   
 	}
 	
